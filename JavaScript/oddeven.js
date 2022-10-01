353397f718bf785f937810e259b153b4c712bf0d
Abinash Boruah <71007608+rockkidd23@users.noreply.github.com>
2022-10-01 13:49:51 +0530

    Create oddeven.js

---
+++ JavaScript/oddeven.js
@@ -0,0 +1,14 @@
+function oddeven(i){
+    
+    if(i==0){
+        console.log(i + " neither is odd nor even");
+    }
+    else if(i%2==0){
+        console.log(i + " is even");
+    }
+    else{
+       console.log(i + " is odd"); 
+    }
+}
+
+oddeven(3);
