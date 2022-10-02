35127d99c8324231a7bbc1f60250a01efd6361bd
vivek00007 <vivektavish@gmail.com>
2022-10-02 22:14:55 +0400

    Added a small website in HTML folder

---
+++ HTML/menu.php
@@ -0,0 +1,24 @@
+<div id="navbar">
+
+  
+  
+  <a class="active" href="home.php">HOME</a>
+  
+  <a href="stock.php">STOCK</a>
+  
+  <a href="specialoffers.php">SPECIAL OFFERS</a>
+  
+  <a href="testdrive.php">TEST DRIVE</a>
+  
+  <a href="review.php">REVIEW</a>
+  
+  <a href="contact.php">CONTACT</a>
+  
+  <a href="aboutus.php">ABOUT US</a>
+  
+  <li style="float:right">
+  <a href="login.php">LOGIN</a></li>
+  
+</div>
+ 
+ 
